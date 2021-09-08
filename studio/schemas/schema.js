fa88def7218208d6715ec69a191bfb50df8cbdf0
Arne Hassel <arne.hassel@gmail.com>
2021-09-08 16:54:45 +0200

    Added schema for titles
    These are a core part of the domain models for Ifi-ordenen

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -10,6 +10,7 @@ import category from './category'
 import post from './post'
 import author from './author'
 import comment from './comment'
+import title from './title'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -26,6 +27,7 @@ export default createSchema({
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
-    comment
+    comment,
+    title
   ])
 })
---
+++ studio/schemas/title.js
@@ -0,0 +1,53 @@
+export default {
+  name: 'title',
+  title: 'Title',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'insignia',
+      title: 'Insignia (Medallion Name)',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: []
+        }
+      ]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image'
+    }
+  }
+}
