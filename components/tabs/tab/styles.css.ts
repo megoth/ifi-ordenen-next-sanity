c4d47192fd7190db914711ba9666d7b52fb974a4
Arne Hassel <arne.hassel@gmail.com>
2021-12-15 08:15:01 +0100

    Trying to improve the tabs on Members page

--- components/tabs/tab/styles.css.ts
+++ components/tabs/tab/styles.css.ts
@@ -2,27 +2,35 @@ import { style } from "@vanilla-extract/css";
 import { vars } from "../../styles.css";
 
 const focusHoverRule = {
-  backgroundColor: vars.color.white,
-  borderColor: vars.color.white,
-  color: vars.color.black,
+  borderColor: vars.color.text,
+  color: vars.color.text,
 };
 export const tabLinkStyle = style({
-  border: "solid 1px",
+  border: "solid 2px",
+  borderColor: vars.color.gray,
+  color: vars.color.gray,
   display: "block",
   padding: "0.5em 1em",
   textDecoration: "none",
   ":first-child": {
     borderRadius: "1em 0 0 1em",
+    borderRightWidth: 0,
   },
   ":last-child": {
     borderRadius: "0 1em 1em 0",
+    borderLeftWidth: 0,
   },
   ":focus": focusHoverRule,
   ":hover": focusHoverRule,
 });
 
 export const tabLinkSelectedStyle = style({
-  backgroundColor: vars.color.green,
-  borderColor: vars.color.green,
-  color: vars.color.black,
+  borderColor: vars.color.text,
+  color: vars.color.text,
+  ":first-child": {
+    borderRightWidth: 2,
+  },
+  ":last-child": {
+    borderLeftWidth: 2,
+  },
 });
