b9a1372585bce67425dfafe2fe2a04434f3cd87d
Arne Hassel <arne.hassel@gmail.com>
2023-01-06 15:39:02 +0100

    Added exportTrailingSlash to nextConfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  exportTrailingSlash: true,
+};
 
 module.exports = withVanillaExtract(nextConfig);
