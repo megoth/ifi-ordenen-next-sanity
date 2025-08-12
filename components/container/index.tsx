d7730d11ec6a8a5c3c81a55d8b8c527af0d7f85e
Arne Hassel <arne.hassel@gmail.com>
2025-08-12 12:59:10 +0200

    Adding id to events
    So users don't have to navigate anew after clicking a filter on the association page

--- components/container/index.tsx
+++ components/container/index.tsx
@@ -5,12 +5,13 @@ import cn from "classnames";
 interface Props {
   children: ReactNode;
   className?: string;
+  id?: string;
   variant?: "brown" | "green";
 }
 
 const Container = forwardRef<HTMLDivElement, Props>(
-  ({ children, className, variant }, ref) => (
-    <div className={cn(containerStyle, variant)} ref={ref}>
+  ({ children, className, variant, ...props }, ref) => (
+    <div className={cn(containerStyle, variant)} ref={ref} {...props}>
       <div className={cn(innerStyle, className)}>{children}</div>
     </div>
   )
