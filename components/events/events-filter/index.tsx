19bc0f175161740c33aeedcdb4d2a631884a694e
Arne Hassel <arne.hassel@gmail.com>
2025-08-12 11:55:41 +0200

    Fixed faulty links in events filter

--- components/events/events-filter/index.tsx
+++ components/events/events-filter/index.tsx
@@ -1,9 +1,10 @@
 import Tabs from "../../tabs";
 import Tab from "../../tabs/tab";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
 
 export default function EventsFilter() {
+  const pathname = usePathname();
   const searchParams = useSearchParams();
   const selectedFilters = searchParams.getAll("filter");
   const tabs = [
@@ -13,13 +14,14 @@ export default function EventsFilter() {
   ];
 
   const isSelected = useCallback((filter: string) => selectedFilters.indexOf(filter) !== -1, [selectedFilters]);
-  const getLink = (filter: string) => isSelected(filter) ? "?" : `?filter=${filter}`;
+  const getQuery = (filter: string) => isSelected(filter) ? {} : { filter };
 
   return (
     <Tabs>
-      <Tab href={"?"} selected={selectedFilters.length === 0}>Alt</Tab>
+      <Tab href={{ pathname }} selected={selectedFilters.length === 0}>Alt</Tab>
       {tabs.map((tab) => (
-        <Tab key={`filter-${tab.filter}`} href={getLink(tab.filter)} selected={isSelected(tab.filter)}>{tab.title}</Tab>
+        <Tab key={`filter-${tab.filter}`} href={{ pathname, query: getQuery(tab.filter) }}
+             selected={isSelected(tab.filter)}>{tab.title}</Tab>
       ))}
     </Tabs>
   );
--- components/link/index.tsx
+++ components/link/index.tsx
@@ -1,34 +1,39 @@
-import React, {MouseEventHandler, ReactNode} from "react";
+import React, { HTMLProps, MouseEventHandler, ReactNode } from "react";
 import NextLink from "next/link";
-import {LinkQuery} from "../../lib/api/link";
-import {LinkProps} from "next/dist/client/link";
+import { LinkQuery } from "../../lib/api/link";
+import { LinkProps } from "next/dist/client/link";
+import { UrlObject } from "url";
 
-interface Props extends LinkQuery, Omit<LinkProps, "href"> {
-    children?: ReactNode;
-    className?: string;
-    href?: string;
-    onClick?: MouseEventHandler<HTMLAnchorElement>;
-    style?: Object;
+export type LinkUrl = string | UrlObject;
+
+interface Props extends Omit<LinkProps, "href">, Omit<HTMLProps<HTMLAnchorElement>, "as" | "href"> {
+  children?: ReactNode;
+  className?: string;
+  href?: LinkUrl;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+  slug?: string;
+  text?: string;
+  url?: string;
 }
 
 export default function Link({
-                                 children,
-                                 className,
-                                 href,
-                                 onClick,
-                                 slug,
-                                 text,
-                                 url,
-                                 style,
-                                 ...props
+                               children,
+                               className,
+                               href,
+                               onClick,
+                               slug,
+                               text,
+                               url,
+                               style,
+                               ...props
                              }: Props) {
-    return (
-        <NextLink href={href || (slug && `/${slug}`) || url}
-                  className={className}
-                  style={style}
-                  onClick={onClick}
-                  {...props}>
-            {text || children}
-        </NextLink>
-    );
+  return (
+    <NextLink href={href || (slug && `/${slug}`) || url}
+              className={className}
+              style={style}
+              onClick={onClick}
+              {...props}>
+      {text || children}
+    </NextLink>
+  );
 }
